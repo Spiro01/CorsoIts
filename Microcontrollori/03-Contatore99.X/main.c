a22bc74a3411a3e2c2e05d63b9ef92f318561d6d
Spiro <riccardospironelli@gmail.com>
2022-11-29 21:06:24 +0100

    Completato esercizio contatore99

--- Microcontrollori/03-Contatore99.X/main.c
+++ Microcontrollori/03-Contatore99.X/main.c
@@ -21,28 +21,17 @@
 #define _XTAL_FREQ 8000000
 
 char conteggio = 99;
-char display, statoConteggio = 0;
+char display, statoConteggio,conteggioFinito = 0;
 
 void main(void) {
     INTCON = 0xA0; // GIE = 1; T0IE = 1;
-    OPTION_REG = 0x86; // PS2 = 1; PS1 = 1; PS0 = 0;
+    OPTION_REG = 0x84; // PS2 = 1; PS1 = 0; PS0 = 0;
     TRISB = 0x00;
     TRISA = 0x0E;
     char button, old_button;
 
     while (1) {
 
-        button = PORTA & 0x02;
-        if (!button && old_button) {
-            __delay_ms(20);
-            button = PORTA & 0x02;
-            if (!button && old_button) {
-                statoConteggio ^= 1;
-            }
-        }
-
-        old_button = button;
-        
         if (!display) {
             writeDisplay(conteggio / 10, 0);
 
@@ -50,6 +39,49 @@ void main(void) {
             writeDisplay(conteggio % 10, 1);
 
         }
+        if(conteggioFinito) continue; //Durante il ciclo di conteggio finito i pulsanti sono inibiti;
+
+        button = PORTA & 0x02;
+        if (!button) {
+            __delay_ms(20);
+            button = PORTA & 0x02;
+            if (!button && old_button) {
+                statoConteggio ^= 1;
+            }
+            old_button = 0;
+            continue;
+        }
+
+        button = PORTA & 0x04;
+        if (!button) {
+            __delay_ms(20);
+            button = PORTA & 0x04;
+            if (!button && old_button && !statoConteggio) {
+                if (conteggio >= 99) conteggio = 99;
+                conteggio++;
+            }
+
+            old_button = 0;
+            __debug_break();
+            continue;
+        }
+
+        button = PORTA & 0x08;
+        if (!button) {
+            __delay_ms(20);
+            button = PORTA & 0x08;
+            if (!button && old_button && !statoConteggio) {
+                conteggio--;
+                if (conteggio <= 1) conteggio = 1;
+            }
+            old_button = 0;
+            continue;
+        }
+
+        old_button = 1;
+
+
+
     }
 }
 
@@ -58,19 +90,30 @@ void __interrupt() ISR(void) {
         INTCONbits.T0IF = 0;
         TMR0 = 6;
         display ^= 1;
-        static char InterruptCounter;
-
+        static unsigned char InterruptCounter;
+        static unsigned int InterruptCounter2;
         InterruptCounter++;
 
-        if (InterruptCounter >= 25) {
+        if (InterruptCounter >= 50) { //Impostare a 250 se si vuole che il timer decrementi una volta al secondo
 
             InterruptCounter = 0;
 
             if (statoConteggio)conteggio--;
-            if (conteggio < 0) conteggio = 99;
-
+            if (conteggio <= 0) {
+                statoConteggio = 0;
+                conteggioFinito = 1;
+            }
         }
 
-
+        if (conteggioFinito) {
+            InterruptCounter2++;
+            PORTAbits.RA0 = 1;
+            if (InterruptCounter2 >= 1250) {
+                InterruptCounter2 = 0;
+                conteggioFinito = 0;
+                PORTAbits.RA0 = 0;
+                conteggio = 99;
+            }
+        }
     }
 }
